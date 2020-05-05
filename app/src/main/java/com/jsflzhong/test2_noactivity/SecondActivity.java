be8043d43559a180dc1bf42fe9ea497dec4e6f93
jsflzhong <jsflzhong@163.com>
2020-05-06 00:13:19 +0800

    intent: send value to last activity -onBackPressed()

--- app/src/main/java/com/jsflzhong/test2_noactivity/SecondActivity.java
+++ app/src/main/java/com/jsflzhong/test2_noactivity/SecondActivity.java
@@ -31,8 +31,11 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     /**
-     * 加载第七个button,事件中: 把要向上个活动传递的数据,放入intent,
+     * 加载第八个button,事件中: 把要向上个活动传递的数据,放入intent,
      *  然后调用finish()销毁本活动, 然后触发上个活动中的onActivityResult()方法.
+     *
+     *  但是如果用户是通过点击返回键,来返回上一个活动的话,则此方法不会被触发.
+     *  但是会触发下面的onBackPressed()方法
      *
      */
     private void loadButton8() {
@@ -48,5 +51,17 @@ public class SecondActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    /**
+     * 当用户点击返回键时,带数据给上一个activity.
+     */
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("returnKey", "send testValue to firstActivity");
+        //向上个活动传递数据
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
