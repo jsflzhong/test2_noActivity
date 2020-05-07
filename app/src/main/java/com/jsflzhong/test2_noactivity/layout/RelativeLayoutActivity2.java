830d21949009ffdb1ab8b194423c134694716e42
jsflzhong <jsflzhong@163.com>
2020-05-07 16:36:25 +0800

    RelativeLayoutActivity

---
+++ app/src/main/java/com/jsflzhong/test2_noactivity/layout/RelativeLayoutActivity2.java
@@ -0,0 +1,16 @@
+package com.jsflzhong.test2_noactivity.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jsflzhong.test2_noactivity.R;
+
+public class RelativeLayoutActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative2_layout);
+    }
+}
